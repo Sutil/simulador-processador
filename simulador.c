02f33a28ac8b236d5d8c6df49c26e44f029b08ef
Sutil <sutil.edu@gmail.com>
2016-07-31 16:42:59 -0300

    implementando clico

--- simulador.c
+++ simulador.c
@@ -9,8 +9,23 @@ char memoria[100][30];
 int iniciouCarregamentoDasInstrucoes = 0;
 int contadorDeInstrucoes = 0;
 
+char* instrucao;
+
 //********** REGISTRADORES ******************
-int PC;
+
+int A; // Acumulador
+int B; //: Uso geral
+int M1; //: Multiplicador
+int M2; //: Multiplicando
+int D1; //: Divisor
+int D2; //: Dividendo
+int R; //: Resto da divisão
+int C; //: Carry out (1: houve carry – 2: não houve carry)
+int Z; //: Resultado zero(-1: resultado negativo; 0: resultado igual a zero; 1: resultado positivo)
+int PC; //: Program Counter
+char* IR; //: Instruction Register
+int MAR; //: Memory Address Register
+char* MBR; //: Memory Buffer Register
 
 // *************** IMPRIMEM A SITUAÇÃO DA MEMÓRIA EM TELA ****************************
 void mostraMemoria(){
@@ -18,6 +33,24 @@ void mostraMemoria(){
     int i;
     for(i = 0; i<100;i++)
         printf("%d>> %s\n", i, memoria[i]);
+}
+
+//************** IMPRIME A SITUAÇÃO DOS REGISTRADORES ******************
+void mostraRegistradores(){
+    printf("\n\n>>>>>>>>>>>>>>>> REGISTRADORES <<<<<<<<<<<<<<<\n\n");
+    printf("A: %d\n",A); // Acumulador
+    printf("B: %d\n",B); //: Uso geral
+    printf("M1: %d\n",M1); //: Multiplicador
+    printf("M2: %d\n",M2); //: Multiplicando
+    printf("D1: %d\n",D1); //: Divisor
+    printf("D2: %d\n",D2); //: Dividendo
+    printf("R: %d\n",R); //: Resto da divisão
+    printf("C: %d\n",C); //: Carry out (1: houve carry – 2: não houve carry)
+    printf("Z: %d\n",Z); //: Resultado zero(-1: resultado negativo; 0: resultado igual a zero; 1: resultado positivo)
+    printf("PC: %d\n",PC); //: Program Counter
+    printf("IR: %s\n",IR); //: Instruction Register
+    printf("MAR: %d\n",MAR); //: Memory Address Register
+    printf("MBR: %s\n",MBR); //: Memory Buffer Register
 }
 
 int hexaToInt(char* hexa){
@@ -88,11 +121,32 @@ void carregaArquivo(){
 
 //************************** FIM CARREGAMENTO DO ARQUIVO*********************
 
+void buscaInstrucao(){
+    MAR = PC;
+    MBR = memoria[MAR];
+    IR = MBR;
+    PC++;
+}
+
+void decodificaInstrucao(){
+    char *token = strtok(IR," ,\t");
+    instrucao = token;
+}
+
+void executaPrograma(){
+    buscaInstrucao();
+    decodificaInstrucao();
+}
+
 int main() {
-    mostraMemoria();
     carregaArquivo();
-    mostraMemoria();
+
     printf("O Programa foi carregado e está pronto para ser executado.");
+
+    executaPrograma();
+
+    mostraMemoria();
+    mostraRegistradores();
 
     return 0;
 }
